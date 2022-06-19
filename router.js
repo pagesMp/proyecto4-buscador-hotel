b056493cc3e5a8d55eb6b788bca61af4db6ab871
pagesMp <marc.msp97@gmail.com>
2022-06-19 22:48:28 +0200

    fechas

--- router.js
+++ router.js
@@ -1,9 +1,14 @@
+
 const router = require('express').Router();
 
-const reservasRouter = require('./routes/reservasRouter');
+//Importamos Routes definidas en views
+const ReservaRouter = require('./routes/reservasRouter');
 
 
-
-router.use('/api', reservasRouter);
+//Rutas
+router.use('/cliente', ReservaRouter);
+router.use('/hotel', ReservaRouter);
+router.use('/reserva', ReservaRouter);
+router.use('/fechas', ReservaRouter);
 
 module.exports = router;
